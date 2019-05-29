dba7f5920e5669f6ec5aa8bab631d72b81882985
Matias Lavik <matias.lavik@hotmail.com>
2019-05-29 21:40:05 +0200

    memcpy routine

--- memory.asm
+++ memory.asm
@@ -1,12 +1,32 @@
 ; *** MEMORY HELPERS ***
 
+; -------------------- [ Routine: memcpy ] --------------------
+; -----copies a block of memory from source to destination-----
+; input: [esp+6] destination (address)
+; input: [esp+4] source (address)
+; input: [esp+2] size (in bytes)
 memcpy:
-  ; TODO
+  mov cx, 0x0000 ; it
+  _memcpy_loop:
+    ; set source
+    mov bx, [esp+4]
+    add bx, cx
+    mov al, [bx]
+    ; set destination
+    mov bx, [esp+6]
+    add bx, cx
+    ; copy value to destination
+    mov byte [bx], al
+    ; increment counter
+    inc cx
+    mov ax, [esp+2]
+    cmp cx, ax
+    jl _memcpy_loop
   ret
 
 ; -------------------- [ Routine: memset ] --------------------
 ; -----ifills a block with memory with the specified value-----
-; input: [esp+6] target address
+; input: [esp+6] destination (address)
 ; input: [esp+4] word size (in bytes)
 ; input: [esp+2] byte value
 memset:
